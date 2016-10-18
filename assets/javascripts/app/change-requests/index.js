e451f0580c93580719d485bbbd61b62968abb00d
emboss <martin.bosslet@gmail.com>
2016-10-18 13:49:46 +0200

    Add more actions section

--- assets/javascripts/app/change-requests/index.js
+++ assets/javascripts/app/change-requests/index.js
@@ -2,7 +2,13 @@ import $ from 'jquery';
 import runInitializer from 'lib/run-initializer';
 
 runInitializer('.change_requests.index', () => {
-  $('.tiles .tile-status').click(function() {
+  $('.tiles .tile-status').click(function(evt) {
+    evt.preventDefault();
     $(this).closest('.tiles').toggleClass('active');
+  });
+
+  $('.tile .toggle-more-actions').click(function(evt) {
+    evt.preventDefault();
+    $(this).closest('.tile').find('.more-actions').toggleClass('active');
   });
 });
