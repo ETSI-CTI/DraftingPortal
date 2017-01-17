96f27cde884d085f6b253ad16c530d2f72e56435
emboss <martin.bosslet@gmail.com>
2017-01-17 07:01:49 +0100

    Allow TB Official to accept contributions or not accept them

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,8 @@ class User
     "#{first_name} #{last_name}"
   end
 
+  def tb_official?
+    id == :tb_official
+  end
+
 end
--- assets/javascripts/app/change-requests/contributions.js
+++ assets/javascripts/app/change-requests/contributions.js
@@ -17,8 +17,40 @@ function toggleContributionsFor($input) {
   });
 }
 
+function updateContribution(evt, contributionClass, iconClass) {
+  const $contribution = $(evt.target).closest('.contribution');
+  ['contributed', 'drafting'].forEach(cls => {
+    $contribution.removeClass(cls);
+  });
+  $contribution.addClass(contributionClass);
+  $contribution.find('.status .fa').addClass(iconClass);
+  $contribution.find('.fa').off('click');
+}
+
+function handleAccept(evt) {
+  updateContribution(evt, 'accepted', 'fa-check-circle');
+}
+
+function handleNotAccept(evt) {
+  updateContribution(evt, 'not-accepted', 'fa-times-circle');
+}
+
+function handleVote(evt, increment) {
+  const span = $(evt.target).siblings('span');
+  span.html(
+    Math.max(0, parseInt(span.html(), 10) + 1)
+  );
+}
+
 runInitializer('.change_requests.contributions', () => {
   $('.contribution .toggle').click(handleToggle);
+  $('.accept').click(handleAccept);
+  $('.not-accept').click(handleNotAccept);
+  ['drafting', 'contributed'].forEach(state => {
+    $(`.${state} .fa-thumbs-up`).click(handleVote);
+    $(`.${state} .fa-thumbs-down`).click(handleVote);
+  });
+
   // show contributions for selected, use timeout to give respec time to render
   window.setTimeout(() => {
     toggleContributionsFor($('.selected .toggle').prop('checked', true));
